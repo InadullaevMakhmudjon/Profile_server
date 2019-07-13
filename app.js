a90c179480f80eaa3c6b9a41eb7a725a9952c176
Makhmudjon <maxmudinadullayev98@gmail.com>
2019-07-13 11:33:53 +0500

    Bindded

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(join(__dirname, 'public')));
+app.use(express.static(join(__dirname, '../profile_client/dist')));
 app.use(cors());
 indexRouter(app);
 
